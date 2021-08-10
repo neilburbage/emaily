8252ef1ac2434ef4e951fccd04105cf40846173b
Neil Burbage <neiljb1975@gmail.com>
2021-08-10 18:20:49 +0100

    changed greeting

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ Ciao: 'bella' });
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
