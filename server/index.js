fe2988b2a3ef23e8862bf96281bb47020e385d10
Neil Burbage <neiljb1975@gmail.com>
2021-09-07 18:31:33 +0100

    fix connect mongoose, implement saving model instances and Mongoose queries

--- server/index.js
+++ server/index.js
@@ -1,32 +1,5 @@
-// index.js component is for pulling libraries and frameworks into the app build out re: to do the heavy lifting
-// index.js is for booting up our application
-// Don't need to use keys.js as database url doesn't contain user name and password.
-// Solution 1:
-// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
-// Solution 2:
-// mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
-// catch(error => handleError(error));
-// Solution 3:
-// try {
-// await mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
-// } catch (error) {
-//   handleError(error);
-// } 
-// Solution 4: 
-// mongoose.connect('mongodb://localhost:27017/game-exampleDB', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Solution 5: 
-// mongoose.connect('mongodb://86.176.115.254/32/nodeReactFullstackWebDev', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Solution 6:
-
-// mongoose.connect('keys.url');
-
-// Solution 7:
-
-// mongoose.connect('keys.URI', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Solution 8:
+// index.js component is for pulling libraries and frameworks into the app build out 
+// re: to do the heavy lifting index.js is for booting up this application
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -34,9 +7,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-url = 'mongodb+srv://emaily:8865zoLipCIC2VIV@cluster0.6tblh.mongodb.net/game-example?retryWrites=true&w=majority';
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect(keys.url, { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
 require('./routes/authRoutes')(app);
 
